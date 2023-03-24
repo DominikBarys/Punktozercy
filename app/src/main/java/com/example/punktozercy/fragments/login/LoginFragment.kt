a6d74e30eee74b93fffe13cd96c433341a2129cd
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-03-24 21:48:30 +0100

    Working at login and register fragments

---
+++ app/src/main/java/com/example/punktozercy/fragments/login/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.example.punktozercy.fragments.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.punktozercy.R
+import com.example.punktozercy.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater,container,false)
+
+        //TODO
+        //binding.button1.setOnClickListener()
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        return binding.root
+    }
+
+}
