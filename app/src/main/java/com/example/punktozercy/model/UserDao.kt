d1871929c408641d3b13e4c1736e83f7b6857736
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-03-31 00:38:46 +0200

    added query for finding user by token

--- app/src/main/java/com/example/punktozercy/model/UserDao.kt
+++ app/src/main/java/com/example/punktozercy/model/UserDao.kt
@@ -34,4 +34,7 @@ interface UserDao {
 
     @Query("SELECT EXISTS(SELECT email,password FROM users WHERE :em = email AND :pass = password)")
     fun isUserLoginExists(em: String,pass:String):Boolean
+
+    @Query("SELECT * FROM users WHERE :token = google_token")
+    fun findUserByGoogleToken(token:String):User
 }
