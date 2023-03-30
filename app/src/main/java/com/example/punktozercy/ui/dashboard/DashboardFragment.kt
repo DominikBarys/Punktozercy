f0fb5e29161b535a43395251c66f3452fe09a90b
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-03-30 18:03:31 +0200

    testing google data

--- app/src/main/java/com/example/punktozercy/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/punktozercy/ui/dashboard/DashboardFragment.kt
@@ -33,7 +33,9 @@ class DashboardFragment : Fragment() {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
-        Toast.makeText(requireContext(),userViewModel.getUser().email,Toast.LENGTH_LONG).show()
+        Toast.makeText(requireContext(),userViewModel.getUser().points.toString(),Toast.LENGTH_LONG).show()
+     //   Toast.makeText(requireContext(),userViewModel.getUser().userName,Toast.LENGTH_LONG).show()
+
 
        // val user = bundle!!.get
         val root: View = binding.root
