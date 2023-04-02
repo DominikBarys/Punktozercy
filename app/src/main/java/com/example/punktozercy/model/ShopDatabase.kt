5fb8fdfeda7f41ecd5379c6de0e33cd50375fecb
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-04-02 02:00:20 +0200

    UserWithProdcutsRef deleted

--- app/src/main/java/com/example/punktozercy/model/ShopDatabase.kt
+++ app/src/main/java/com/example/punktozercy/model/ShopDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class,Product::class,ShoppingHistory::class,UserProductCrossRef::class], version = 1, exportSchema = false)
+@Database(entities = [User::class,Product::class,ShoppingHistory::class], version = 1, exportSchema = false)
 abstract class ShopDatabase: RoomDatabase() {
 
     abstract fun userDao():UserDao
