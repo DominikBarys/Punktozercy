b26a8acf6ef67aaaf13055e58812db9f2f082bf4
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-04-10 21:11:24 +0200

    added toast for testing user

--- app/src/main/java/com/example/punktozercy/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/punktozercy/fragments/home/HomeFragment.kt
@@ -6,17 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.punktozercy.databinding.FragmentHomeBinding
 import com.example.punktozercy.mainscreen.MainScreenViewModel
+import com.example.punktozercy.viewModel.UserViewModel
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
   //  private var mainScreenVm = activity?.let { ViewModelProvider(it)[MainScreenViewModel::class.java] }
    private lateinit var mainScreenVm: MainScreenViewModel
-
+    private lateinit var userViewModel: UserViewModel
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -30,6 +32,7 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this)[HomeViewModel::class.java]
 
         mainScreenVm = ViewModelProvider(requireActivity())[MainScreenViewModel::class.java]
+        userViewModel = ViewModelProvider(requireActivity())[UserViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -40,7 +43,7 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
 
-
+        Toast.makeText(requireContext(), userViewModel.getUser().userName, Toast.LENGTH_SHORT).show()
         binding.debugAddProductButton.setOnClickListener{
             mainScreenVm.amountOfProductsInBasket.value = mainScreenVm.amountOfProductsInBasket.value!! + 1
         }
