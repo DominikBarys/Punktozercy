51cdc38e7a8b284f95eb2e019473df0f7fbe50cb
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-04-13 23:50:28 +0200

    added simple products data for initialization

--- app/src/main/java/com/example/punktozercy/model/ShopDatabase.kt
+++ app/src/main/java/com/example/punktozercy/model/ShopDatabase.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 
 @Database(entities = [User::class,Product::class,ShoppingHistory::class], version = 1, exportSchema = false)
 abstract class ShopDatabase: RoomDatabase() {
@@ -13,9 +18,42 @@ abstract class ShopDatabase: RoomDatabase() {
     abstract fun shoppingHistoryDao():ShoppingHistoryDao
 
     companion object{
+        private val products :List<Product> = listOf(
+            Product(0, "Butter", 5.99, "Butter made from cow's milk", "dairy", null, "images/butter.jpg"),
+            Product(0, "Country Sausage", 24.99, "Traditional country sausage made from pork and bacon", "meat", 500, "images/sausage.jpg"),
+            Product(0, "Sauerkraut", 3.99, "Sauerkraut preserved in brine", "vegetables", 100, "images/sauerkraut.jpg"),
+            Product(0, "Rye Bread", 4.49, "Whole grain rye bread", "bakery", null, "images/rye_bread.jpg"),
+            Product(0, "Mead", 19.99, "Natural mead with a smoky flavor", "beverages", 800, "images/mead.jpg"),
+            Product(0, "Blue Cheese", 29.99, "Assorted blue cheeses", "dairy", null, "images/blue_cheese.jpg"),
+            Product(0, "Buckwheat Groats", 2.49, "Unroasted buckwheat groats", "cereals", 50, "images/buckwheat_groats.jpg"),
+            Product(0, "Ruskie Pierogi", 9.99, "Pierogi with potato and cheese filling", "ready meals", null, "images/pierogi.jpg"),
+            Product(0, "Toruń Gingerbread", 12.99, "Traditional gingerbread from Toruń", "sweets", 200, "images/gingerbread.jpg"),
+            Product(0, "Apple Juice", 4.99, "100% apple juice", "beverages", null, "images/apple_juice.jpg"),
+            Product(0, "Goat Cheese", 14.99, "Goat cheese made from goat's milk", "dairy", 300, "images/goat_cheese.jpg"),
+            Product(0, "Homemade Sausage", 18.99, "Sausage made from pork and beef", "meat", null, "images/homemade_sausage.jpg"),
+            Product(0, "Pickled Cucumbers", 2.99, "Pickled cucumbers in brine", "vegetables", 50, "images/pickled_cucumbers.jpg"),
+            Product(0, "Whole Bean Coffee", 19.99, "Whole bean coffee made from a blend of Arabica and Robusta", "beverages", 500, "images/coffee.jpg"),
+            Product(0, "Feta Cheese", 9.99, "Feta cheese made from cow's milk", "dairy", null, "images/feta_cheese.jpg"),
+            Product(0, "Wheat Beer", 4.99, "Craft wheat beer", "beverages", 100, "images/wheat_beer.jpg"),
+            Product(0, "Blood Sausage", 8.99, "Blood sausage made from pork blood and fat", "meat", null, "images/blood_sausage.jpg"))
 
         @Volatile
         private var INSTANCE: ShopDatabase? = null;
+
+        // Klasa Callback do zainicjalizowania tabeli danymi przed jej stworzeniem
+        private class DatabaseCallback(private val context: Context, private val products: List<Product>) : RoomDatabase.Callback() {
+            @OptIn(DelicateCoroutinesApi::class)
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                // Wykonywanie kodu przy tworzeniu bazy danych
+                // Można tutaj zainicjalizować tabelę danymi
+                val productDao = ShopDatabase.getDatabase(context).productDao()
+                GlobalScope.launch {
+                    productDao.insertProducts(products)
+                }
+
+            }
+        }
 
         fun getDatabase(context:Context): ShopDatabase{
             val tempInstance = INSTANCE
@@ -27,7 +65,8 @@ abstract class ShopDatabase: RoomDatabase() {
                     context.applicationContext,
                     ShopDatabase::class.java,
                     "shop_database"
-                ).build()
+                ).addCallback(DatabaseCallback(context, products))
+                    .build()
                 INSTANCE= instance
                 return instance
             }
