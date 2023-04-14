537c17866b2b5fe417c15171f1682efb0ae01f69
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-04-14 03:15:22 +0200

    implemented recycle view and loading products by category

--- app/src/main/java/com/example/punktozercy/fragments/home/productslist/ProductViewModel.kt
+++ app/src/main/java/com/example/punktozercy/fragments/home/productslist/ProductViewModel.kt
@@ -2,16 +2,27 @@ package com.example.punktozercy.fragments.home.productslist
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.punktozercy.model.Product
 
 class ProductViewModel: ViewModel() {
 
+
+
     companion object{
         var selectedCategory: String = "None"
+        lateinit var productList : List<Product>
+    }
+    fun setProductList(products:List<Product>){
+        productList = products
+    }
+    fun getProductList():List<Product>{
+        return productList
     }
 
     fun setCategory(cat: String){
         selectedCategory = cat
     }
+
 
     fun getCategory(): String{
         return selectedCategory
--- app/src/main/java/com/example/punktozercy/fragments/home/productslist/ProductsListFragment.kt
+++ app/src/main/java/com/example/punktozercy/fragments/home/productslist/ProductsListFragment.kt
@@ -1,6 +1,8 @@
 package com.example.punktozercy.fragments.home.productslist
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,16 +10,24 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.punktozercy.R
 import com.example.punktozercy.databinding.FragmentProductsListBinding
 import com.example.punktozercy.fragments.home.HomeViewModel
+import com.example.punktozercy.fragments.home.adapters.ProductsAdapter
+import com.example.punktozercy.model.Product
+import com.example.punktozercy.viewModel.ShopViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class ProductsListFragment : Fragment() {
 
     private var _binding: FragmentProductsListBinding? = null
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var productViewModel: ProductViewModel
+    private lateinit var mShopViewModel: ShopViewModel
 
     private val binding get() = _binding!!
 
@@ -25,13 +35,29 @@ class ProductsListFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentProductsListBinding.inflate(inflater, container, false)
         homeViewModel = ViewModelProvider(requireActivity())[HomeViewModel::class.java]
         productViewModel = ViewModelProvider(this)[ProductViewModel::class.java]
+        mShopViewModel = ViewModelProvider(requireActivity())[ShopViewModel::class.java]
 
-        binding.textView6.text = productViewModel.getCategory()
+        lifecycleScope.launch {
+           val job =  lifecycleScope.async {
+                 return@async  productViewModel.setProductList(mShopViewModel.getProductsByCategory(productViewModel.getCategory()))
+
+           }
+            job.await()
+
+            val produkty:List<Product> = productViewModel.getProductList()
+            Toast.makeText(requireContext(), productViewModel.getProductList().size.toString(), Toast.LENGTH_SHORT).show()
+            val adapter = ProductsAdapter(productViewModel.getProductList(),requireContext())
+            for(products in produkty){
+                Log.d("MORENKA",products.name)
+            }
+            binding.productsView.layoutManager = LinearLayoutManager(requireContext())
+            binding.productsView.adapter = adapter
+        }
 
         binding.backButton.setOnClickListener {
             Navigation.findNavController(requireView()).navigateUp()
