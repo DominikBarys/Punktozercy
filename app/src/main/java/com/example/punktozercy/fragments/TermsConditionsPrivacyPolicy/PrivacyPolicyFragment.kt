8c5d10292fa4161d94840af06ffccc61d9c20807
Brystol <odido160@gmail.com>
2023-04-21 23:20:46 +0200

    Other design changes

--- app/src/main/java/com/example/punktozercy/fragments/TermsConditionsPrivacyPolicy/PrivacyPolicyFragment.kt
+++ app/src/main/java/com/example/punktozercy/fragments/TermsConditionsPrivacyPolicy/PrivacyPolicyFragment.kt
@@ -1,19 +1,35 @@
 package com.example.punktozercy.fragments.TermsConditionsPrivacyPolicy
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.punktozercy.R
+import com.example.punktozercy.databinding.FragmentPrivacyPolicyBinding
 
 class PrivacyPolicyFragment : Fragment() {
+
+    private var _binding: FragmentPrivacyPolicyBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_privacy_policy, container, false)
+
+        _binding = FragmentPrivacyPolicyBinding.inflate(inflater, container, false)
+
+        binding.privacyPolicyText.movementMethod = ScrollingMovementMethod()
+
+        binding.acceptButton.setOnClickListener{
+            Navigation.findNavController(requireView()).navigateUp()
+        }
+
+
+
+        return binding.root
     }
 }
